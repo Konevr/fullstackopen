99424002e0d6478afeb1170f414597813c852283
Konstantinos Evripidou <44362176+Konevr@users.noreply.github.com>
2020-11-19 14:04:57 +0000

    merged develop

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -36,22 +36,6 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-  // return (
-  //   <div>
-  //     <h1>{course}</h1>
-  //     <p>
-  //       {part1} {exercises1}
-  //     </p>
-  //     <p>
-  //       {part2} {exercises2}
-  //     </p>
-  //     <p>
-  //       {part3} {exercises3}
-  //     </p>
-  //     <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-  //   </div>
-  // )
-
   return (
     <div>
       <Header course={course} />

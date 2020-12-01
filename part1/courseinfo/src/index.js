b47eedc9a951ec90b85cb65cbb95a6700831e678
Konstantinos Evripidou <44362176+Konevr@users.noreply.github.com>
2020-12-01 14:54:16 +0000

    fixed commit bug

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -43,32 +43,6 @@ const Total = (props) => {
 };
 
 const App = () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-=======
-=======
->>>>>>> develop
   const course = {
     name: "Half Stack application development",
     parts: [
@@ -92,10 +66,6 @@ const App = () => {
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
-<<<<<<< HEAD
->>>>>>> develop
-=======
->>>>>>> develop
     </div>
   );
 };
